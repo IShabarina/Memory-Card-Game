8960eb964477dbc8586e4c750a421f34f55a39d4
irinashabarina <irina.potashevskaya@gmail.com>
2021-12-01 15:40:39 +0300

    HW 4.8 updated, task 2, 4

--- 04_arrays-and-loops/task2_flip.js
+++ 04_arrays-and-loops/task2_flip.js
@@ -2,10 +2,16 @@
 // произвольной строки
 
 let str = 'Привет, мир!!!';
-let flipStr = '';
 
-for (i = str.length - 1; i >= 0; i--) {
-  flipStr = flipStr + str[i];
-}
+// Вариант 1
+console.log(str.split('').reverse().join(''));
 
-console.log(flipStr);
+// Вариант 2
+// let flipStr = '';
+
+// for (i = str.length - 1; i >= 0; i--) {
+//   flipStr = flipStr + str[i];
+// }
+// console.log(flipStr);
+
+
--- 04_arrays-and-loops/task4_calendar.js
+++ 04_arrays-and-loops/task4_calendar.js
@@ -4,21 +4,26 @@
 
 let number = [];
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let dayOfWeek = 'Воскресенье';
+let dayOfWeek = 'Вторник';
 
-let d = week.indexOf(dayOfWeek); //1
+// Вариант 1
+// let d = week.indexOf(dayOfWeek); //1
+
+// for (i = 0; i < 31; ++i) {
+//   number[i] = i + 1;
+//   console.log(number[i], 'января, ', week[d]);
+//   if (d < 6) {
+//     ++d;
+//   } else {
+//     d = 0;
+//   }
+// }
+
+
+// Вариант 2
 
 for (i = 0; i < 31; ++i) {
   number[i] = i + 1;
-  console.log(number[i], 'января, ', week[d]);
-
-  if (d < 6) {
-    ++d;
-  } else {
-    d = 0;
-  }
+  const index = number[i] % 7;
+  console.log(number[i], 'января, ', week[Math.abs(index - 1 + week.indexOf(dayOfWeek))]);
 }
-
-
-
-
