cbfa65878818c13f31e517d76148de1abbdc7e38
Shabira <81320062+IShabarina@users.noreply.github.com>
2022-11-07 13:14:03 +0300

    Delete 03_string-boolean-conditionals directory

--- 03_string-boolean-conditionals/task1_password.js
+++
@@ -1,3 +0,0 @@
-let password = '____-----';
-let passwordValidator = (password.length >= 4 && (password.includes("_") || password.includes("-"))) ? 'Пароль надёжный' : 'Пароль недостаточно надёжный';
-console.log(passwordValidator);
